cdafefe5d72c5ab134e8fc86a5bbc2c0254af613
Andrew Savetchuk <andrew.savetchuk@gmail.com>
2021-11-25 18:57:26 +0200

    chore: add and configure prettier

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,20 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
         <title>Andrew Savetchuk</title>
-        <meta name="description" content="Software Engineer with a background in developing web applications and services for global customers, across the Consumer Services, Medical, and Fashion industries." />
-        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👨🏻‍💻</text></svg>" />
+        <meta
+          name="description"
+          content="Software Engineer with a background in developing web applications and services for global customers, across the Consumer Services, Medical, and Fashion industries."
+        />
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👨🏻‍💻</text></svg>"
+        />
       </Head>
-
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
@@ -21,5 +26,5 @@ export default function Home() {
         </p>
       </main>
     </div>
-  )
+  );
 }
