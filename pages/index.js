3444e21d3905e556488da84f1273da219e4479ab
Andrew Savetchuk <andrew.savetchuk@gmail.com>
2021-11-27 13:25:51 +0200

    refactor(homepage): remove unused components and styles

--- pages/index.js
+++ pages/index.js
@@ -15,16 +15,7 @@ export default function Home() {
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👨🏻‍💻</text></svg>"
         />
       </Head>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-      </main>
+      <main className={styles.main}></main>
     </div>
   );
 }
