39aa8cd6a0691394e9c18b42af80cb94756ae842
Andrew Savetchuk <andrew.savetchuk@gmail.com>
2021-12-03 19:41:45 +0200

    feat: add intro and links sections

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,8 @@
 import Head from 'next/head';
+import Image from 'next/image';
+import ReactHtmlParser from 'react-html-parser';
+
+import linkBlocks from '../config/linkBlocks.json';
 import styles from '../styles/Home.module.scss';
 
 export default function Home() {
@@ -15,7 +19,62 @@ export default function Home() {
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👨🏻‍💻</text></svg>"
         />
       </Head>
-      <main className={styles.main}></main>
+      <main className={styles.main}>
+        <div className={styles.mainColumn}>
+          <section className={styles.intro}>
+            <Image
+              src="/img/avatar.jpg"
+              className={styles.introAvatar}
+              alt="Andrew Savetchuk's avatar"
+              width={120}
+              height={120}
+              quality={100}
+              priority={true}
+            />
+            <h1 className={styles.introTitle}>Andrew Savetchuk</h1>
+            <p className={styles.introDescription}>
+              Software Engineer, Content Creator,
+              <br /> Open-Source contributor
+            </p>
+            <a
+              href="mailto:andrew.savetchuk@gmail.com"
+              className={styles.introButton}
+            >
+              <span>&#x1F4E8;</span>Get In Touch
+            </a>
+          </section>
+          <section className={styles.links}>
+            {linkBlocks.map((linkBlock, linkBlockIndex) => (
+              <div
+                key={`linkBlock-${linkBlockIndex}`}
+                className={styles.linksBlock}
+              >
+                <h2>{linkBlock.title}</h2>
+                <ul>
+                  {linkBlock.links?.length
+                    ? linkBlock.links.map((link, linkIndex) => (
+                        <li key={`linkBlock-${linkBlockIndex}-${linkIndex}`}>
+                          <div>
+                            <span>{ReactHtmlParser(link.icon)}</span>
+                            <a
+                              href={link.href}
+                              target="_blank"
+                              rel="noreferrer"
+                              className={styles.linksItemTitle}
+                            >
+                              {link.title}
+                            </a>
+                            <div className={styles.linksItemSubtitle}></div>
+                          </div>
+                        </li>
+                      ))
+                    : ''}
+                </ul>
+              </div>
+            ))}
+          </section>
+        </div>
+      </main>
     </div>
   );
 }
