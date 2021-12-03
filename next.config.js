fdfb0e50ba8ce1aab36b438cf4bfa7f92dd40c62
Andrew Savetchuk <andrew.savetchuk@gmail.com>
2021-12-03 19:46:06 +0200

    feat: add lang attribute to html

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 const path = require('path');
 
 module.exports = {
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
   reactStrictMode: true,
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
