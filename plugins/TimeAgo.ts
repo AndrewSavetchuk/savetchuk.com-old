7133e4b978cac8aeeebba90c842865282f406218
Andrew Savetchuk <andrew.savetchuk@gmail.com>
2023-03-17 00:30:32 -0400

    refactor: remove unused plugins and services

--- plugins/TimeAgo.ts
+++
@@ -1,7 +0,0 @@
-import TimeAgo from 'javascript-time-ago';
-import en from 'javascript-time-ago/locale/en';
-
-TimeAgo.setDefaultLocale(en.locale);
-TimeAgo.addLocale(en);
-
-export default new TimeAgo('en-US');
